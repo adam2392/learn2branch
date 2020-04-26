836dc2353ad7747f817173e7020cb02887021211
v715 <vgopala4@jhu.edu>
2020-04-26 18:43:07 -0400

    Reformat with Black

--- 02_generate_dataset.py
+++ 02_generate_dataset.py
@@ -12,8 +12,17 @@ import utilities
 
 
 class SamplingAgent(scip.Branchrule):
-
-    def __init__(self, episode, instance, seed, out_queue, exploration_policy, query_expert_prob, out_dir, follow_expert=True):
+    def __init__(
+        self,
+        episode,
+        instance,
+        seed,
+        out_queue,
+        exploration_policy,
+        query_expert_prob,
+        out_dir,
+        follow_expert=True,
+    ):
         self.episode = episode
         self.instance = instance
         self.seed = seed
@@ -34,20 +43,29 @@ class SamplingAgent(scip.Branchrule):
 
         if self.model.getNNodes() == 1:
             # initialize root buffer for Khalil features extraction
-            utilities.extract_khalil_variable_features(self.model, [], self.khalil_root_buffer)
+            utilities.extract_khalil_variable_features(
+                self.model, [], self.khalil_root_buffer
+            )
 
         # once in a while, also run the expert policy and record the (state, action) pair
         query_expert = self.rng.rand() < self.query_expert_prob
         if query_expert:
             state = utilities.extract_state(self.model)
             cands, *_ = self.model.getPseudoBranchCands()
-            state_khalil = utilities.extract_khalil_variable_features(self.model, cands, self.khalil_root_buffer)
+            state_khalil = utilities.extract_khalil_variable_features(
+                self.model, cands, self.khalil_root_buffer
+            )
 
-            result = self.model.executeBranchRule('vanillafullstrong', allowaddcons)
+            result = self.model.executeBranchRule("vanillafullstrong", allowaddcons)
             cands_, scores, npriocands, bestcand = self.model.getVanillafullstrongData()
 
             assert result == scip.SCIP_RESULT.DIDNOTRUN
-            assert all([c1.getCol().getLPPos() == c2.getCol().getLPPos() for c1, c2 in zip(cands, cands_)])
+            assert all(
+                [
+                    c1.getCol().getLPPos() == c2.getCol().getLPPos()
+                    for c1, c2 in zip(cands, cands_)
+                ]
+            )
 
             action_set = [c.getCol().getLPPos() for c in cands]
             expert_action = action_set[bestcand]
@@ -57,35 +75,48 @@ class SamplingAgent(scip.Branchrule):
             # Do not record inconsistent scores. May happen if SCIP was early stopped (time limit).
             if not any([s < 0 for s in scores]):
 
-                filename = f'{self.out_dir}/sample_{self.episode}_{self.sample_counter}.pkl'
-                with gzip.open(filename, 'wb') as f:
-                    pickle.dump({
-                        'episode': self.episode,
-                        'instance': self.instance,
-                        'seed': self.seed,
-                        'node_number': self.model.getCurrentNode().getNumber(),
-                        'node_depth': self.model.getCurrentNode().getDepth(),
-                        'data': data,
-                        }, f)
+                filename = (
+                    f"{self.out_dir}/sample_{self.episode}_{self.sample_counter}.pkl"
+                )
+                with gzip.open(filename, "wb") as f:
+                    pickle.dump(
+                        {
+                            "episode": self.episode,
+                            "instance": self.instance,
+                            "seed": self.seed,
+                            "node_number": self.model.getCurrentNode().getNumber(),
+                            "node_depth": self.model.getCurrentNode().getDepth(),
+                            "data": data,
+                        },
+                        f,
+                    )
 
-                self.out_queue.put({
-                    'type': 'sample',
-                    'episode': self.episode,
-                    'instance': self.instance,
-                    'seed': self.seed,
-                    'node_number': self.model.getCurrentNode().getNumber(),
-                    'node_depth': self.model.getCurrentNode().getDepth(),
-                    'filename': filename,
-                })
+                self.out_queue.put(
+                    {
+                        "type": "sample",
+                        "episode": self.episode,
+                        "instance": self.instance,
+                        "seed": self.seed,
+                        "node_number": self.model.getCurrentNode().getNumber(),
+                        "node_depth": self.model.getCurrentNode().getDepth(),
+                        "filename": filename,
+                    }
+                )
 
                 self.sample_counter += 1
 
         # if exploration and expert policies are the same, prevent running it twice
-        if not query_expert or (not self.follow_expert and self.exploration_policy != 'vanillafullstrong'):
+        if not query_expert or (
+            not self.follow_expert and self.exploration_policy != "vanillafullstrong"
+        ):
             result = self.model.executeBranchRule(self.exploration_policy, allowaddcons)
 
         # apply 'vanillafullstrong' branching decision if needed
-        if query_expert and self.follow_expert or self.exploration_policy == 'vanillafullstrong':
+        if (
+            query_expert
+            and self.follow_expert
+            or self.exploration_policy == "vanillafullstrong"
+        ):
             assert result == scip.SCIP_RESULT.DIDNOTRUN
             cands, scores, npriocands, bestcand = self.model.getVanillafullstrongData()
             self.model.branchVar(cands[bestcand])
@@ -107,15 +138,25 @@ def make_samples(in_queue, out_queue):
     """
 
     while True:
-        episode, instance, seed, exploration_policy, query_expert_prob, time_limit, out_dir = in_queue.get()
-        print(f'[w {os.getpid()}] episode {episode}, seed {seed}, processing instance \'{instance}\'...')
+        (
+            episode,
+            instance,
+            seed,
+            exploration_policy,
+            query_expert_prob,
+            time_limit,
+            out_dir,
+        ) = in_queue.get()
+        print(
+            f"[w {os.getpid()}] episode {episode}, seed {seed}, processing instance '{instance}'..."
+        )
 
         m = scip.Model()
-        m.setIntParam('display/verblevel', 0)
-        m.readProblem(f'{instance}')
+        m.setIntParam("display/verblevel", 0)
+        m.readProblem(f"{instance}")
         utilities.init_scip_params(m, seed=seed)
-        m.setIntParam('timing/clocktype', 2)
-        m.setRealParam('limits/time', time_limit)
+        m.setIntParam("timing/clocktype", 2)
+        m.setRealParam("limits/time", time_limit)
 
         branchrule = SamplingAgent(
             episode=episode,
@@ -124,40 +165,49 @@ def make_samples(in_queue, out_queue):
             out_queue=out_queue,
             exploration_policy=exploration_policy,
             query_expert_prob=query_expert_prob,
-            out_dir=out_dir)
+            out_dir=out_dir,
+        )
 
         m.includeBranchrule(
             branchrule=branchrule,
-            name="Sampling branching rule", desc="",
-            priority=666666, maxdepth=-1, maxbounddist=1)
+            name="Sampling branching rule",
+            desc="",
+            priority=666666,
+            maxdepth=-1,
+            maxbounddist=1,
+        )
 
-        m.setBoolParam('branching/vanillafullstrong/integralcands', True)
-        m.setBoolParam('branching/vanillafullstrong/scoreall', True)
-        m.setBoolParam('branching/vanillafullstrong/collectscores', True)
-        m.setBoolParam('branching/vanillafullstrong/donotbranch', True)
-        m.setBoolParam('branching/vanillafullstrong/idempotent', True)
+        m.setBoolParam("branching/vanillafullstrong/integralcands", True)
+        m.setBoolParam("branching/vanillafullstrong/scoreall", True)
+        m.setBoolParam("branching/vanillafullstrong/collectscores", True)
+        m.setBoolParam("branching/vanillafullstrong/donotbranch", True)
+        m.setBoolParam("branching/vanillafullstrong/idempotent", True)
 
-        out_queue.put({
-            'type': 'start',
-            'episode': episode,
-            'instance': instance,
-            'seed': seed,
-        })
+        out_queue.put(
+            {"type": "start", "episode": episode, "instance": instance, "seed": seed,}
+        )
 
         m.optimize()
         m.freeProb()
 
-        print(f"[w {os.getpid()}] episode {episode} done, {branchrule.sample_counter} samples")
+        print(
+            f"[w {os.getpid()}] episode {episode} done, {branchrule.sample_counter} samples"
+        )
 
-        out_queue.put({
-            'type': 'done',
-            'episode': episode,
-            'instance': instance,
-            'seed': seed,
-        })
+        out_queue.put(
+            {"type": "done", "episode": episode, "instance": instance, "seed": seed,}
+        )
 
 
-def send_orders(orders_queue, instances, seed, exploration_policy, query_expert_prob, time_limit, out_dir):
+def send_orders(
+    orders_queue,
+    instances,
+    seed,
+    exploration_policy,
+    query_expert_prob,
+    time_limit,
+    out_dir,
+):
     """
     Continuously send sampling orders to workers (relies on limited
     queue capacity).
@@ -184,13 +234,31 @@ def send_orders(orders_queue, instances, seed, exploration_policy, query_expert_
     episode = 0
     while True:
         instance = rng.choice(instances)
-        seed = rng.randint(2**32)
-        orders_queue.put([episode, instance, seed, exploration_policy, query_expert_prob, time_limit, out_dir])
+        seed = rng.randint(2 ** 32)
+        orders_queue.put(
+            [
+                episode,
+                instance,
+                seed,
+                exploration_policy,
+                query_expert_prob,
+                time_limit,
+                out_dir,
+            ]
+        )
         episode += 1
 
 
-def collect_samples(instances, out_dir, rng, n_samples, n_jobs,
-                    exploration_policy, query_expert_prob, time_limit):
+def collect_samples(
+    instances,
+    out_dir,
+    rng,
+    n_samples,
+    n_jobs,
+    exploration_policy,
+    query_expert_prob,
+    time_limit,
+):
     """
     Runs branch-and-bound episodes on the given set of instances, and collects
     randomly (state, action) pairs from the 'vanilla-fullstrong' expert
@@ -219,25 +287,33 @@ def collect_samples(instances, out_dir, rng, n_samples, n_jobs,
     os.makedirs(out_dir, exist_ok=True)
 
     # start workers
-    orders_queue = mp.Queue(maxsize=2*n_jobs)
+    orders_queue = mp.Queue(maxsize=2 * n_jobs)
     answers_queue = mp.SimpleQueue()
     workers = []
     for i in range(n_jobs):
         p = mp.Process(
-                target=make_samples,
-                args=(orders_queue, answers_queue),
-                daemon=True)
+            target=make_samples, args=(orders_queue, answers_queue), daemon=True
+        )
         workers.append(p)
         p.start()
 
-    tmp_samples_dir = f'{out_dir}/tmp'
+    tmp_samples_dir = f"{out_dir}/tmp"
     os.makedirs(tmp_samples_dir, exist_ok=True)
 
     # start dispatcher
     dispatcher = mp.Process(
-            target=send_orders,
-            args=(orders_queue, instances, rng.randint(2**32), exploration_policy, query_expert_prob, time_limit, tmp_samples_dir),
-            daemon=True)
+        target=send_orders,
+        args=(
+            orders_queue,
+            instances,
+            rng.randint(2 ** 32),
+            exploration_policy,
+            query_expert_prob,
+            time_limit,
+            tmp_samples_dir,
+        ),
+        daemon=True,
+    )
     dispatcher.start()
 
     # record answers and write samples
@@ -249,11 +325,11 @@ def collect_samples(instances, out_dir, rng, n_samples, n_jobs,
         sample = answers_queue.get()
 
         # add received sample to buffer
-        if sample['type'] == 'start':
-            buffer[sample['episode']] = []
+        if sample["type"] == "start":
+            buffer[sample["episode"]] = []
         else:
-            buffer[sample['episode']].append(sample)
-            if sample['type'] == 'sample':
+            buffer[sample["episode"]].append(sample)
+            if sample["type"] == "sample":
                 in_buffer += 1
 
         # if any, write samples from current episode
@@ -264,16 +340,18 @@ def collect_samples(instances, out_dir, rng, n_samples, n_jobs,
             for sample in samples_to_write:
 
                 # if no more samples here, move to next episode
-                if sample['type'] == 'done':
+                if sample["type"] == "done":
                     del buffer[current_episode]
                     current_episode += 1
 
                 # else write sample
                 else:
-                    os.rename(sample['filename'], f'{out_dir}/sample_{i+1}.pkl')
+                    os.rename(sample["filename"], f"{out_dir}/sample_{i+1}.pkl")
                     in_buffer -= 1
                     i += 1
-                    print(f"[m {os.getpid()}] {i} / {n_samples} samples written, ep {sample['episode']} ({in_buffer} in buffer).")
+                    print(
+                        f"[m {os.getpid()}] {i} / {n_samples} samples written, ep {sample['episode']} ({in_buffer} in buffer)."
+                    )
 
                     # early stop dispatcher (hard)
                     if in_buffer + i >= n_samples and dispatcher.is_alive():
@@ -292,24 +370,22 @@ def collect_samples(instances, out_dir, rng, n_samples, n_jobs,
     shutil.rmtree(tmp_samples_dir, ignore_errors=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'problem',
-        help='MILP instance type to process.',
-        choices=['setcover', 'cauctions', 'facilities', 'indset'],
+        "problem",
+        help="MILP instance type to process.",
+        choices=["setcover", "cauctions", "facilities", "indset"],
     )
     parser.add_argument(
-        '-s', '--seed',
-        help='Random generator seed.',
+        "-s",
+        "--seed",
+        help="Random generator seed.",
         type=utilities.valid_seed,
         default=0,
     )
     parser.add_argument(
-        '-j', '--njobs',
-        help='Number of parallel jobs.',
-        type=int,
-        default=1,
+        "-j", "--njobs", help="Number of parallel jobs.", type=int, default=1,
     )
     args = parser.parse_args()
 
@@ -318,33 +394,37 @@ if __name__ == '__main__':
     train_size = 100000
     valid_size = 20000
     test_size = 20000
-    exploration_strategy = 'pscost'
+    exploration_strategy = "pscost"
     node_record_prob = 0.05
     time_limit = 3600
 
-    if args.problem == 'setcover':
-        instances_train = glob.glob('data/instances/setcover/train_500r_1000c_0.05d/*.lp')
-        instances_valid = glob.glob('data/instances/setcover/valid_500r_1000c_0.05d/*.lp')
-        instances_test = glob.glob('data/instances/setcover/test_500r_1000c_0.05d/*.lp')
-        out_dir = 'data/samples/setcover/500r_1000c_0.05d'
+    if args.problem == "setcover":
+        instances_train = glob.glob(
+            "data/instances/setcover/train_500r_1000c_0.05d/*.lp"
+        )
+        instances_valid = glob.glob(
+            "data/instances/setcover/valid_500r_1000c_0.05d/*.lp"
+        )
+        instances_test = glob.glob("data/instances/setcover/test_500r_1000c_0.05d/*.lp")
+        out_dir = "data/samples/setcover/500r_1000c_0.05d"
 
-    elif args.problem == 'cauctions':
-        instances_train = glob.glob('data/instances/cauctions/train_100_500/*.lp')
-        instances_valid = glob.glob('data/instances/cauctions/valid_100_500/*.lp')
-        instances_test = glob.glob('data/instances/cauctions/test_100_500/*.lp')
-        out_dir = 'data/samples/cauctions/100_500'
+    elif args.problem == "cauctions":
+        instances_train = glob.glob("data/instances/cauctions/train_100_500/*.lp")
+        instances_valid = glob.glob("data/instances/cauctions/valid_100_500/*.lp")
+        instances_test = glob.glob("data/instances/cauctions/test_100_500/*.lp")
+        out_dir = "data/samples/cauctions/100_500"
 
-    elif args.problem == 'indset':
-        instances_train = glob.glob('data/instances/indset/train_500_4/*.lp')
-        instances_valid = glob.glob('data/instances/indset/valid_500_4/*.lp')
-        instances_test = glob.glob('data/instances/indset/test_500_4/*.lp')
-        out_dir = 'data/samples/indset/500_4'
+    elif args.problem == "indset":
+        instances_train = glob.glob("data/instances/indset/train_500_4/*.lp")
+        instances_valid = glob.glob("data/instances/indset/valid_500_4/*.lp")
+        instances_test = glob.glob("data/instances/indset/test_500_4/*.lp")
+        out_dir = "data/samples/indset/500_4"
 
-    elif args.problem == 'facilities':
-        instances_train = glob.glob('data/instances/facilities/train_100_100_5/*.lp')
-        instances_valid = glob.glob('data/instances/facilities/valid_100_100_5/*.lp')
-        instances_test = glob.glob('data/instances/facilities/test_100_100_5/*.lp')
-        out_dir = 'data/samples/facilities/100_100_5'
+    elif args.problem == "facilities":
+        instances_train = glob.glob("data/instances/facilities/train_100_100_5/*.lp")
+        instances_valid = glob.glob("data/instances/facilities/valid_100_100_5/*.lp")
+        instances_test = glob.glob("data/instances/facilities/test_100_100_5/*.lp")
+        out_dir = "data/samples/facilities/100_100_5"
         time_limit = 600
 
     else:
@@ -358,19 +438,38 @@ if __name__ == '__main__':
     os.makedirs(out_dir)
 
     rng = np.random.RandomState(args.seed)
-    collect_samples(instances_train, out_dir + '/train', rng, train_size,
-                    args.njobs, exploration_policy=exploration_strategy,
-                    query_expert_prob=node_record_prob,
-                    time_limit=time_limit)
+    collect_samples(
+        instances_train,
+        out_dir + "/train",
+        rng,
+        train_size,
+        args.njobs,
+        exploration_policy=exploration_strategy,
+        query_expert_prob=node_record_prob,
+        time_limit=time_limit,
+    )
 
     rng = np.random.RandomState(args.seed + 1)
-    collect_samples(instances_valid, out_dir + '/valid', rng, test_size,
-                    args.njobs, exploration_policy=exploration_strategy,
-                    query_expert_prob=node_record_prob,
-                    time_limit=time_limit)
+    collect_samples(
+        instances_valid,
+        out_dir + "/valid",
+        rng,
+        test_size,
+        args.njobs,
+        exploration_policy=exploration_strategy,
+        query_expert_prob=node_record_prob,
+        time_limit=time_limit,
+    )
 
     rng = np.random.RandomState(args.seed + 2)
-    collect_samples(instances_test, out_dir + '/test', rng, test_size,
-                    args.njobs, exploration_policy=exploration_strategy,
-                    query_expert_prob=node_record_prob,
-                    time_limit=time_limit)
+    collect_samples(
+        instances_test,
+        out_dir + "/test",
+        rng,
+        test_size,
+        args.njobs,
+        exploration_policy=exploration_strategy,
+        query_expert_prob=node_record_prob,
+        time_limit=time_limit,
+    )
+
