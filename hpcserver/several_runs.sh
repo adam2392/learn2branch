4e2059276ac0b5285cb35fe8cb691f4a24c62181
mfigdore <mfigdor1@jhu.edu>
2020-04-30 10:03:54 -0400

    batch runs

--- hpcserver/several_runs.sh
+++ hpcserver/several_runs.sh
@@ -3,6 +3,6 @@
 problems=( setcover cauctions indset tsp )
 for ((i = 0; i < ${#problems[@]}; ++i)); do
     # bash arrays are 0-indexed
-    position=$(( $i + 1 ))
-    sbatch run_training_jobs_2_bulk.sh ${problems[$i]} 10$position
+    #position=$(( $i + 1 ))
+    sbatch working_run.sh ${problems[$i]} --seed 211
 done
