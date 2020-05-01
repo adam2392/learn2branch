36ae44162c8573a5fd4038185569b3fa04f7881b
Adam Li <adam2392@gmail.com>
2020-04-30 22:31:14 -0400

    Adding updates and figures.

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -3,6 +3,9 @@ import numpy as np
 import collections
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 def _strip_line(line):
     try:
         line = line.split("]")[1]
@@ -16,7 +19,7 @@ def _summarize_line(line):
     acc3 = line.split("acc@3: ")[1].split(" ")[0]
     acc5 = line.split("acc@5: ")[1].split(" ")[0]
     acc10 = line.split("acc@10: ")[1].split(" ")[0]
-    return (loss, acc1, acc3, acc5, acc10)
+    return list(map(float, (loss, acc1, acc3, acc5, acc10)))
 
 def _read_log_file(fpath):
 
@@ -40,11 +43,36 @@ def _read_log_file(fpath):
             elif 'TRAIN' in line:
                 summary = _summarize_line(line)
                 train_summary[epoch_ind].append(summary)
-            elif 'VALID' in line:
+            elif 'VALID' in line and 'BEST' not in line:
                 summary = _summarize_line(line)
                 test_summary[epoch_ind].append(summary)
             line = _strip_line(fin.readline())
     return summary_txt, train_summary, test_summary
+
+def plot_summary(train_summary, test_summary, fig_fpath):
+    fig, axs = plt.subplots(1, 2)
+    epochs = list(train_summary.keys())
+    epochs = list(test_summary.keys())
+
+    titles = ['Training', 'Testing']
+    for i, (summary, titlestr) in enumerate(zip([train_summary, test_summary], titles)):
+        summ_arr = np.array(list(summary.values())).squeeze()
+        curve_labels = ['loss', 'acc1', 'acc3', 'acc5', 'acc10']
+
+        ax = axs[i]
+        for j, label in enumerate(curve_labels):
+            ax.plot(summ_arr[:, j], label=label)
+        ax.set_ylabel("Value")
+        ax.set_xlabel("Epoch")
+        ax.set_title(f"{titlestr} Summary")
+        ax.legend()
+
+    plt.savefig(fig_fpath)
+    fig.show()
+    return fig, axs
+
+
+
 
 if __name__ == '__main__':
     datadir = Path(Path(os.getcwd()).parent / "trained_models")
@@ -55,6 +83,7 @@ if __name__ == '__main__':
 
         seed_dirs = [x for x in problem_dir.glob("*") if not x.name.startswith(".")]
         for seed_dir in seed_dirs:
+            seed = os.path.basename(seed_dir)
             print(problem, seed_dir)
             # seed_dir = Path(problem_dir / seed)
             best_params_fpath = Path(seed_dir / 'best_params.pkl')
@@ -62,7 +91,13 @@ if __name__ == '__main__':
                 log_fpath = Path(seed_dir / 'log.txt')
                 summary_txt, train_summary, test_summary = _read_log_file(log_fpath)
 
-                print("done")
-                print(train_summary)
-                print(test_summary)
+                fig_fpath = Path(Path("./figures/") / f"{problem}-{seed}-trainvalidsummary.png")
+                fig_fpath.parent.mkdir(parents=True, exist_ok=True)
+                # print("done")
+                # print(train_summary)
+                # print(test_summary)
+                plot_summary(train_summary, test_summary, fig_fpath)
+
+                # break
+
         # break
