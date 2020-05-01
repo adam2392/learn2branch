600e01511fd52175a2b31ead5d1c9375f4782bd4
Adam Li <adam2392@gmail.com>
2020-04-30 21:16:05 -0400

    Adding updated eval.

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -4,12 +4,27 @@ import collections
 from pathlib import Path
 
 def _strip_line(line):
-    return line.split("]")[1]
+    try:
+        line = line.split("]")[1]
+    except:
+        line = None
+    return line
+
+def _summarize_line(line):
+    loss = line.split("LOSS: ")[1].split(" ")[0]
+    acc1 = line.split("acc@1: ")[1].split(" ")[0]
+    acc3 = line.split("acc@3: ")[1].split(" ")[0]
+    acc5 = line.split("acc@5: ")[1].split(" ")[0]
+    acc10 = line.split("acc@10: ")[1].split(" ")[0]
+    return (loss, acc1, acc3, acc5, acc10)
 
 def _read_log_file(fpath):
 
     summary_txt = []
-    train_summary = collections
+    summary_txt.append(['loss', 'acc@1', 'acc@3', 'acc@5', 'acc@10'])
+    train_summary = collections.defaultdict(list)
+    test_summary = collections.defaultdict(list)
+
     with open(fpath, 'r') as fin:
         # read in summary of the training
         line = _strip_line(fin.readline())
@@ -17,12 +32,19 @@ def _read_log_file(fpath):
             summary_txt.append(line)
             line = _strip_line(fin.readline())
 
+        epoch_ind = 0
         # read train/validation per epoch
-        while fin:
-
-
-    print(summary_txt)
-    return summary_txt
+        while line:
+            if 'EPOCH' in line:
+                epoch_ind += 1
+            elif 'TRAIN' in line:
+                summary = _summarize_line(line)
+                train_summary[epoch_ind].append(summary)
+            elif 'VALID' in line:
+                summary = _summarize_line(line)
+                test_summary[epoch_ind].append(summary)
+            line = _strip_line(fin.readline())
+    return summary_txt, train_summary, test_summary
 
 if __name__ == '__main__':
     datadir = Path(Path(os.getcwd()).parent / "trained_models")
@@ -31,12 +53,16 @@ if __name__ == '__main__':
     for problem in problems:
         problem_dir = Path(datadir / problem / modelname )
 
-        seeds = [x for x in problem_dir.glob("*") if not x.name.startswith(".")]
-        for seed in seeds:
-            seed_dir = Path(problem_dir / seed)
+        seed_dirs = [x for x in problem_dir.glob("*") if not x.name.startswith(".")]
+        for seed_dir in seed_dirs:
+            print(problem, seed_dir)
+            # seed_dir = Path(problem_dir / seed)
             best_params_fpath = Path(seed_dir / 'best_params.pkl')
             if best_params_fpath in seed_dir.glob("*.pkl"):
                 log_fpath = Path(seed_dir / 'log.txt')
-                _read_log_file(log_fpath)
+                summary_txt, train_summary, test_summary = _read_log_file(log_fpath)
+
                 print("done")
-        break
+                print(train_summary)
+                print(test_summary)
+        # break
